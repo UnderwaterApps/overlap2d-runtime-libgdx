9a7e07cafcfcd1ddcf8896026782006e4f8237f5
Gevorg Kopalyan <gevorg.kopalyan@gmail.com>
2016-01-13 17:58:46 +0400

    bugfix for particles in composite

--- src/com/uwsoft/editor/renderer/systems/ParticleSystem.java
+++ src/com/uwsoft/editor/renderer/systems/ParticleSystem.java
@@ -5,15 +5,12 @@ import com.badlogic.ashley.core.Entity;
 import com.badlogic.ashley.core.Family;
 import com.badlogic.ashley.systems.IteratingSystem;
 import com.badlogic.gdx.graphics.g2d.ParticleEffect;
-import com.uwsoft.editor.renderer.components.DimensionsComponent;
-import com.uwsoft.editor.renderer.components.TransformComponent;
 import com.uwsoft.editor.renderer.components.particle.ParticleComponent;
-import com.uwsoft.editor.renderer.utils.ComponentRetriever;
 
 public class ParticleSystem extends IteratingSystem {
 
 	private ComponentMapper<ParticleComponent> particleComponentMapper = ComponentMapper.getFor(ParticleComponent.class);
-    private ComponentMapper<TransformComponent> transformComponentMapper = ComponentMapper.getFor(TransformComponent.class);
+    //private ComponentMapper<TransformComponent> transformComponentMapper = ComponentMapper.getFor(TransformComponent.class);
     //private ComponentMapper<ParentNodeComponent> parentNodeComponentMapper = ComponentMapper.getFor(ParentNodeComponent.class);
     //private ComponentMapper<NodeComponent> nodeComponentMapper = ComponentMapper.getFor(NodeComponent.class);
 	
@@ -25,12 +22,9 @@ public class ParticleSystem extends IteratingSystem {
 	protected void processEntity(Entity entity, float deltaTime) {
 		//System.out.println("DELTAING  " + entity.getId() +"  " +deltaTime);
 		ParticleComponent particleComponent = particleComponentMapper.get(entity);
-		TransformComponent transformComponent = transformComponentMapper.get(entity);
-		DimensionsComponent dimensionsComponent = ComponentRetriever.get(entity, DimensionsComponent.class);
+
+		//DimensionsComponent dimensionsComponent = ComponentRetriever.get(entity, DimensionsComponent.class);
 		ParticleEffect particleEffect = particleComponent.particleEffect;
-		
-		//particleEffect.setPosition(transformComponent.x/particleComponent.worldMultiplyer, transformComponent.y/particleComponent.worldMultiplyer);
-		particleEffect.setPosition(transformComponent.x, transformComponent.y);
 		particleEffect.update(deltaTime);
 		//ParentNodeComponent parentNodeComponent = parentNodeComponentMapper.get(entity);
 		
