7452c5be48fc427bea70af75b0b3c0871930dc8e
Simone Arpe <simon.arpe@gmail.com>
2016-08-06 21:38:19 +0200

    added shader logic

--- src/com/uwsoft/editor/renderer/components/ShaderComponent.java
+++ src/com/uwsoft/editor/renderer/components/ShaderComponent.java
@@ -1,15 +1,30 @@
 package com.uwsoft.editor.renderer.components;
 
 import com.badlogic.ashley.core.Component;
+import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.glutils.ShaderProgram;
+import com.uwsoft.editor.renderer.systems.render.logic.ShaderLogic;
 
 public class ShaderComponent implements Component {
 	public String shaderName;
 	private ShaderProgram shaderProgram = null;
+    public ShaderLogic shaderLogic;
 
-	public void setShader(String name, ShaderProgram program) {
+
+    public void setShader(String name, ShaderProgram program) {
 		shaderName = name;
 		shaderProgram = program;
+        shaderLogic = new ShaderLogic() {
+            @Override
+            public void begin(Batch batch, ShaderComponent shader) {
+                batch.setShader(shader.getShader());
+            }
+
+            @Override
+            public void end(Batch batch) {
+                batch.setShader(null);
+            }
+        };
 	}
 
 	public ShaderProgram getShader() {
---
+++ src/com/uwsoft/editor/renderer/components/shader/ShaderParameter.java
@@ -0,0 +1,10 @@
+package com.uwsoft.editor.renderer.components.shader;
+
+import com.badlogic.gdx.assets.AssetLoaderParameters;
+import com.badlogic.gdx.graphics.glutils.ShaderProgram;
+
+/**
+ * @author Michael "molp" Olp
+ */
+public class ShaderParameter extends AssetLoaderParameters<ShaderProgram> {
+}
